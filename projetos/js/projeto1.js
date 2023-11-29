3d85dc950cd915f1ef8e86cc2fa93d888d82e32d
Ana Júlia <meloanajulia30@gmail.com>
2023-11-29 18:16:40 -0300

    update

---
+++ projetos/js/projeto1.js
@@ -0,0 +1,9 @@
+let cont = document.querySelector('.container');
+let layer = document.querySelectorAll('.layer');
+
+cont.onscroll = function (){
+  let X = cont.scrollTop;
+
+  layer[0].style.left = X/4 + 'px';
+  layer[1].style.left = X/8 + 'px';
+}
