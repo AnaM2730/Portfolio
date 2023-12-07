0adccd554552275042475fc10d4ce0f0fd140235
Ana Júlia <meloanajulia30@gmail.com>
2023-12-07 17:05:27 -0300

    criacao modal

---
+++ js/janela-modal.js
@@ -0,0 +1,4 @@
+const openModalButton = document.querySelector("#open_modal");  
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
