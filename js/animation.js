22a467e523c269c6cdf197a6391287f6c2589922
Ana Júlia <107368074+AnaM2730@users.noreply.github.com>
2024-01-27 14:06:16 -0300

    update
    rotate = animaton

---
+++ js/animation.js
@@ -0,0 +1,50 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.card-proj1', {duration: 1000});
+sr.reveal('.card-proj2', {duration: 1000});
+sr.reveal('.card-proj3', {duration: 1000});
+sr.reveal('.card-proj4', {duration: 1000});
+sr.reveal('.card-proj5', {duration: 1000});
+sr.reveal('.card-proj6', {duration: 1000});
+sr.reveal('.card-proj7', {duration: 1000});
+sr.reveal('.card-proj8', {duration: 1000});
+sr.reveal('.card-proj9', {duration: 1000});
+sr.reveal('.card-proj10', {duration: 1000});
+
+
+
+
+
+
+/* sr.reveal('.card-proj3', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj4', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj5', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj6', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj7', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj8', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj9', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+});
+sr.reveal('.card-proj10', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+}); */
